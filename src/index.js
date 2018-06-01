56a63eda85431bd927adad41a1b5365c409e65ea
Colin Gourlay <Gourlay.Colin@abc.net.au>
2018-06-01 14:12:18 +1000

    Inline function because the latest aunty's build process would have inlined this and tried to re-assign a const.

--- src/index.js
+++ src/index.js
@@ -10,40 +10,39 @@ const IMG_ATTRIBUTE_PATTERNS = Object.keys(HTMLImageElement.prototype)
     return memo;
   }, {});
 
-function detailPageURLFromId(id) {
-  return `${(window.location.origin || '').replace('mobile', 'www')}/news/${id}?pfm=ms`;
-}
-
 Array.prototype.slice.call(document.querySelectorAll(`a[name^="${NAME_PREFIX}"]`)).forEach(anchorEl => {
   const id = anchorEl.name.substring(NAME_PREFIX.length);
 
-  xhr({ url: detailPageURLFromId(id) }, (err, response, body) => {
-    if (err || response.statusCode !== 200) {
-      return console.error(err || new Error(response.statusCode));
-    }
-
-    // Reselect in case it no longer exists
-    if (!(anchorEl = document.querySelector(`a[name^="${NAME_PREFIX}${id}"]`))) {
-      return;
-    }
-
-    const imgHTML = body.match(new RegExp(`<img [^>]*${id}[^>]*>`))[0];
-
-    const props = Object.keys(IMG_ATTRIBUTE_PATTERNS).reduce((memo, prop) => {
-      const match = imgHTML.match(IMG_ATTRIBUTE_PATTERNS[prop]);
-
-      if (match) {
-        memo[prop] = match[1];
+  xhr(
+    { url: `${(window.location.origin || '').replace('mobile', 'www')}/news/${id}?pfm=ms` },
+    (err, response, body) => {
+      if (err || response.statusCode !== 200) {
+        return console.error(err || new Error(response.statusCode));
       }
 
-      return memo;
-    }, {});
+      // Reselect in case it no longer exists
+      if (!(anchorEl = document.querySelector(`a[name^="${NAME_PREFIX}${id}"]`))) {
+        return;
+      }
 
-    const component = new Image(props);
+      const imgHTML = body.match(new RegExp(`<img [^>]*${id}[^>]*>`))[0];
 
-    anchorEl.parentElement.insertBefore(component.el, anchorEl);
-    anchorEl.parentElement.removeChild(anchorEl);
-  });
+      const props = Object.keys(IMG_ATTRIBUTE_PATTERNS).reduce((memo, prop) => {
+        const match = imgHTML.match(IMG_ATTRIBUTE_PATTERNS[prop]);
+
+        if (match) {
+          memo[prop] = match[1];
+        }
+
+        return memo;
+      }, {});
+
+      const component = new Image(props);
+
+      anchorEl.parentElement.insertBefore(component.el, anchorEl);
+      anchorEl.parentElement.removeChild(anchorEl);
+    }
+  );
 });
 
 if (process.env.NODE_ENV === 'development') {
